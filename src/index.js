53afd783deca11cb2e4659774319ce8ee8d15206
Slava-Shchipunov <shchipunovsd@gmail.com>
2022-01-04 12:03:01 +0700

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+
+    if (n < 0) {
+        n = -n;
+    };
+
+    let str = String(n);
+    let array = str.split("");
+    let reverseArray = array.reverse();
+    let reverseStr = reverseArray.join("");
+    return Number(reverseStr);
 }
